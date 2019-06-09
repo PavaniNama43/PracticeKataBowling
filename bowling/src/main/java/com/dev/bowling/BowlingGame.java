5f4e4030039e46c39cc43a00a41266a9edb029d3
princy43 <pavani.26492@gmail.com>
2019-06-09 23:29:39 +0200

    Added game score when all rolls are gutter balls

---
+++ bowling/src/main/java/com/dev/bowling/BowlingGame.java
@@ -0,0 +1,12 @@
+package com.dev.bowling;
+
+public class BowlingGame {
+
+	public void roll(int pins) {
+		
+	}
+	
+	public int getScore() {
+		return 0;
+	}
+}
--- bowling/src/test/java/com/dev/bowling/BowlingGameTest.java
+++ bowling/src/test/java/com/dev/bowling/BowlingGameTest.java
@@ -1,14 +1,18 @@
 package com.dev.bowling;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
 	@Test
-	public void testInitialSetUp() {
-		assertTrue(true);
+	public void testGameScoreForGutterBalls() {
+		BowlingGame game= new BowlingGame();
+		
+		for(int i=0;i<20;i++) {
+			game.roll(0);
+		}
+		
+		assertEquals(0, game.getScore());
 	}
-
 }
