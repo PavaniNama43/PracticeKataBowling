cce3c0fe9bc47248431bf5ecfc63dbbb4ef23e08
princy43 <pavani.26492@gmail.com>
2019-06-09 23:35:10 +0200

    Added game score when two pins are  down in each roll

--- bowling/src/test/java/com/dev/bowling/BowlingGameTest.java
+++ bowling/src/test/java/com/dev/bowling/BowlingGameTest.java
@@ -15,4 +15,15 @@ public class BowlingGameTest {
 		
 		assertEquals(0, game.getScore());
 	}
+	
+	@Test
+	public void testGameScoreWhenTwoPinsDownPerRoll() {
+		BowlingGame game= new BowlingGame();
+		
+		for(int i=0;i<20;i++) {
+			game.roll(2);
+		}
+		
+		assertEquals(40, game.getScore());
+	}
 }
