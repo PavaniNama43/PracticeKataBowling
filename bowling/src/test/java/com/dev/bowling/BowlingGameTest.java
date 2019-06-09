1c04398ca0c5e133fcc3461bb2a3f1ce2db2ac05
princy43 <pavani.26492@gmail.com>
2019-06-09 23:18:36 +0200

    initial project set up

---
+++ bowling/src/test/java/com/dev/bowling/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package com.dev.bowling;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testInitialSetUp() {
+		assertTrue(true);
+	}
+
+}
